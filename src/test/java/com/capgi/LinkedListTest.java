4cbc6fea0094c2e713d49f63097678afe18cc177
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-12 15:49:34 +0530

    [Shubham]Added: pop last method to delete last element from the linked list

--- src/test/java/com/capgi/LinkedListTest.java
+++ src/test/java/com/capgi/LinkedListTest.java
@@ -6,7 +6,20 @@ import org.junit.Test;
 
 public class LinkedListTest {
 	@Test
-	public void givenThreeNumbersWhenAddedT0LinkedListShouldGetAddedInBack() {
+	public void WhenGivenThreeNumbersShouldPassLinkedListTest() {
+		Node<Integer> firstNode = new Node<Integer>(56);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(70);
+
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
+
+		assertTrue(result);
+	}
+
+	@Test
+	public void givenThreeNumbersWhenAddedT0LinkedListShouldGetAddedInFront() {
 		Node<Integer> firstNode = new Node<Integer>(70);
 		Node<Integer> secondNode = new Node<Integer>(30);
 		Node<Integer> thirdNode = new Node<Integer>(56);
